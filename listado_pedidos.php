7eb1293fdab365a20fe74dbbb713f31acd8d7041
caloloc2 <calolomino@gmail.com>
2019-02-08 23:47:19 -0500

    actualiza

--- listado_pedidos.php
+++ listado_pedidos.php
@@ -1,13 +1,19 @@
 <?php 
 
-require 'meta.php';
 
-$items = Meta::Consulta("SELECT * FROM pedidos ORDER BY id_pedido ASC");
 
-if (count($items)>0){
-    foreach ($items as $linea) {
-		$respuesta[] = $linea;
-	}
+if ((isset($_REQUEST['id_usuario']))&&(!empty($_REQUEST['id_usuario']))){
+		$id_usuario = $_REQUEST['id_usuario'];
+
+		require 'meta.php';
+
+		$items = Meta::Consulta("SELECT * FROM pedidos WHERE id_usuario=".$id_usuario." ORDER BY id_pedido ASC");
+
+		if (count($items)>0){
+				foreach ($items as $linea) {
+				$respuesta[] = $linea;
+			}
+		}
 }
 
 echo json_encode($respuesta);
